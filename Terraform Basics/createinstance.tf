ced797e8b0fcb81e2659408c2b7d3d97891901ae
ManjotSingh531 <manjotsingh77400@gmail.com>
2026-08-23 19:09:38 +0530

    name chnaged

--- Terraform Basics/createinstance.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "myfirstinstance" {
-    count = 3
-    ami = "ami-01a00762f46d584a1"
-    instance_type = "t3.micro"
-
-    tags = {
-        Name = "myfirstinstance-${count.index}"
-    }
-}
--- Terraform Variables/createinstance.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "myfirstinstance" {
-    ami = lookup(var.AMI, var.AWS_REGION)
-    instance_type = "t3.micro"
-
-    tags = {
-        Name = "Gibs"
-    }
-    security_groups = var.SecurityGroup
-}
