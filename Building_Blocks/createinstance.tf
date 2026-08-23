c1bc1496f43bbabacd9b67fa5fe1cb0c379358b2
ManjotSingh531 <manjotsingh77400@gmail.com>
2026-08-23 19:08:42 +0530

    custom instance config

---
+++ Building_Blocks/createinstance.tf
@@ -0,0 +1,34 @@
+resource "aws_key_pair" "flolki_key" {
+    key_name = "floki"
+    public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+resource "aws_instance" "myfirstinstance" {
+    ami = lookup(var.AMI, var.AWS_REGION)
+    instance_type = "t3.micro"
+
+    tags = {
+        Name = "Gibs"
+    }
+
+    security_groups = var.SecurityGroup
+
+    provisioner "file" {
+        source = installNginx.sh
+        destination = "/tmp/installNginx.sh"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "chmod +x /tmp/installNginx.sh",
+            "sudo sed -i -c 's/\\r//' /tmp/installNginx.sh",
+            "/tmp/installNginx.sh"
+        ]
+    }
+
+    connection {
+        host = coalesce(self.public_ip, self.private_ip)
+        type = "ssh"
+        user = var.INSTANCE_USERNAME
+        private_key = file(var.PATH_TO_PRIVATE_KEY)
+    }
+}
---
+++ Building_Blocks/variables.tf
@@ -0,0 +1,30 @@
+variable "AWS_SECRET_KEY"{}
+variable "AWS_REGION" {
+    default = "ap-south-1"
+}
+
+variable "SecurityGroup" {
+    type = list(string)
+    default = ["sg-03e6a051495baff2b", "sg-0b2f243cf09cc5d2f"]
+}
+
+variable "AMI" {
+    default = {
+        "ap-south-1" = "ami-01a00762f46d584a1"
+        "us-east-1" = "ami-0b6d9d3d33ba97d99"
+        "us-east-2" = "ami-0e5497a77ef21b5ac"
+        "us-west-1" = "ami-0fb110df4c5094d21"
+    }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+    default = "floki"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+    default = "floki.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+    default = "ubuntu"
+}
