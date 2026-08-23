92f7710d0e65bbdd237e48203e0eee74454079eb
ManjotSingh531 <manjotsingh77400@gmail.com>
2026-08-23 13:35:41 +0530

    add security groups

--- Terraform Variables/createinstance.tf
+++ Terraform Variables/createinstance.tf
@@ -5,4 +5,5 @@ resource "aws_instance" "myfirstinstance" {
     tags = {
         Name = "Gibs"
     }
+    security_groups = ["${var.SecurityGroup}"]
 }
--- Terraform Variables/variables.tf
+++ Terraform Variables/variables.tf
@@ -3,3 +3,6 @@ variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
     default = "ap-south-1"
 }
+variable "SecurityGroup" {
+    default = ["sg-03e6a051495baff2b", "sg-0b2f243cf09cc5d2f"]
+}
