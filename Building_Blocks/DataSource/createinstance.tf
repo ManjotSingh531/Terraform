543cad1ddbd307963592e26318994ab1e6d25cd5
ManjotSingh531 <manjotsingh77400@gmail.com>
2026-08-24 12:14:27 +0530

    small changes

--- Building_Blocks/DataSource/createinstance.tf
+++ Building_Blocks/DataSource/createinstance.tf
@@ -3,7 +3,7 @@ data "aws_availability_zones" "available" {}
 resource "aws_instance" "myfirstinstance" {
     ami = lookup(var.AMI, var.AWS_REGION)
     instance_type = "t3.micro"
-    availability_zone = data.availability_zones.available.names[0]
+    availability_zone = data.aws_availability_zones.available.names[0]
 
     tags = {
         Name = "Gibs"
