9c15a9d0d66b599355dd4d690ed36bb697ee427a
ManjotSingh531 <manjotsingh77400@gmail.com>
2026-08-24 12:07:01 +0530

    add avialiability zone

--- Building_Blocks/DataSource/createinstance.tf
+++ Building_Blocks/DataSource/createinstance.tf
@@ -1,6 +1,9 @@
+data "availability_zones" "available" {}
+
 resource "aws_instance" "myfirstinstance" {
     ami = lookup(var.AMI, var.AWS_REGION)
     instance_type = "t3.micro"
+    availability_zone = data.availability_zones.available.names[0]
 
     tags = {
         Name = "Gibs"
