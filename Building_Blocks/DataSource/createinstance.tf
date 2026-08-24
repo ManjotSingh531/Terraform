26c7a2e0d5f45aebe6f000e6d0ef40b27dac7ba5
ManjotSingh531 <manjotsingh77400@gmail.com>
2026-08-24 12:52:58 +0530

    add amis

--- Building_Blocks/DataSource/createinstance.tf
+++ Building_Blocks/DataSource/createinstance.tf
@@ -1,7 +1,27 @@
 data "aws_availability_zones" "available" {}
 
+data "aws_ami" "latest_ubuntu" {
+    most_recent = true
+    owners = ["679593333241"]
+
+    filter {
+        name = "name"
+        values = ["ubuntu/images-testing/hvm-ssd-gp3/ubuntu-stonking-daily-amd64-server-*"]
+    }
+
+    filter {
+        name = "architecture"
+        values = ["x86_64"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
 resource "aws_instance" "myfirstinstance" {
-    ami = lookup(var.AMI, var.AWS_REGION)
+    ami = data.aws_ami.latest_ubuntu.id
     instance_type = "t3.micro"
     availability_zone = data.aws_availability_zones.available.names[0]
 
