f5d196fbf3281de1dd36b000c996a22470d32a2a
ManjotSingh531 <manjotsingh77400@gmail.com>
2026-08-29 12:24:04 +0530

    small chages

--- AWS/IAM/iamuser.tf
+++ AWS/IAM/iamuser.tf
@@ -14,15 +14,21 @@ resource "aws_iam_group" "admingroup" {
 
 # Group Assign
 resource "aws_iam_user_group_membership" "admin1_group_membership" {
-  user   = [
-    aws_iam_user.admin1.name,
-    aws_iam_user.admin2.name
+  user = aws_iam_user.admin1.name
+  groups = [
+    aws_iam_group.admingroup.name
   ]
-  groups = [aws_iam_group.admingroup.name]
+}
+
+resource "aws_iam_user_group_membership" "admin2_group_membership" {
+  user = aws_iam_user.admin2.name
+  groups = [
+    aws_iam_group.admingroup.name
+  ]
 }
 
 # Policy Attachment for AWS Groups
 resource "aws_iam_group_policy_attachment" "admin-user-access" {
-    group      = aws_iam_group.admingroup.name
-    policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  group      = aws_iam_group.admingroup.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
