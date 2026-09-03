acf58e783f6cbf69bf70144609f7c554ff71a227
ManjotSingh531 <manjotsingh77400@gmail.com>
2026-09-03 15:19:11 +0530

    changes made

--- AWS/ELB/autoscaling.tf
+++ AWS/ELB/autoscaling.tf
@@ -4,7 +4,7 @@ resource "aws_launch_template" "gibs-launch-template" {
     image_id = lookup(var.AMI, var.AWS_REGION)
     instance_type = "t3.micro"
     key_name = aws_key_pair.gibs-key.key_name
-    security_group_names = [aws_security_group.gibs-ec2-sg.name]
+    vpc_security_group_ids = [aws_security_group.gibs-ec2-sg.id]
     user_data = filebase64("nginx.sh")
 
     lifecycle {
