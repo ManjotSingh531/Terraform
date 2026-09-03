9ce3ae3117f99713d594415115451fa15b38c66a
ManjotSingh531 <manjotsingh77400@gmail.com>
2026-09-03 16:54:32 +0530

    module config

---
+++ Terraform_Modules/main.tf
@@ -0,0 +1,14 @@
+module "ec2_cluster" {
+    source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
+
+    name = "gibs-ec2-instance"
+    instance_count = 2
+    ami = "ami-01a00762f46d584a1"
+    instance_type = "t3.micro"
+    subnet_id = "subnet-01b92eed43b828a5d"
+
+    tags = {
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
---
+++ Terraform_Modules/provider.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+    access_key = "AKIAQSWPC2NSL7Q5N7OA"
+    region = "ap-south-1"
+}
