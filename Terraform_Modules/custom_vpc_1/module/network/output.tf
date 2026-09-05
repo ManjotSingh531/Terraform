30e2dcebbe9e2964dfe5445f667afe57431f2459
ManjotSingh531 <manjotsingh77400@gmail.com>
2026-09-05 20:37:58 +0530

    updated

--- Terraform_Modules/custom_vpc_1/module/network/output.tf
+++ Terraform_Modules/custom_vpc_1/module/network/output.tf
@@ -2,11 +2,10 @@ output "vpc_id" {
     value = aws_vpc.gibs_vpc.id
 }
 
-output "public_subnet_id" {
+output "subnet_id" {
     value = aws_subnet.gibs_subnet.id
 }
 
-output "security_group_id" {
+output "sg_id" {
     value = aws_security_group.gibs_security_group.id
-    
 }
